686db4801bac81ccdb70d5772173cab9bc810d90
0VP0 <ahmad.j.alf@gmail.com>
2024-11-03 00:56:41 +0300

    chore: sign up request dto

---
+++ src/main/java/com/car/carshowroombackend/dto/SignupRequest.java
@@ -0,0 +1,11 @@
+package com.car.carshowroombackend.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    private String email;
+    private String password;
+    private String name;
+}
