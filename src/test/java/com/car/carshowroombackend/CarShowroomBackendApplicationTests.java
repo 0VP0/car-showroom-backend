5550d19314d2b39a71a66ddd8c7418ec568a3a6f
0VP0 <ahmad.j.alf@gmail.com>
2024-11-03 01:03:10 +0300

    chore: comment test files

---
+++ src/test/java/com/car/carshowroombackend/CarShowroomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.car.carshowroombackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CarShowroomBackendApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
